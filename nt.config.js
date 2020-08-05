5659884a2c66ceaf965c43b2b8db215c3d3aedcb
Brent Miller <brentwmiller93@gmail.com>
2020-08-04 22:10:50 -0400

    Adds gapi info

--- nt.config.js
+++ nt.config.js
@@ -1,16 +1,22 @@
 export default {
-  site_name: "New Tabby",
-  description: "Your dashboard on every new tab",
-  theme_color: "#040404",
+  site_name: 'New Tabby',
+  description: 'Your dashboard on every new tab',
+  theme_color: '#040404',
   firebase: {
     config: {
-      apiKey: "AIzaSyD6UhXa92S_dPnTxp-3DrngSHOfU1g7684",
-      authDomain: "tabby-4af5b.firebaseapp.com",
-      databaseURL: "https://tabby-4af5b.firebaseio.com",
-      projectId: "tabby-4af5b",
-      storageBucket: "tabby-4af5b.appspot.com",
-      messagingSenderId: "849564500568",
-      appId: "1:849564500568:web:750ccd709daae80287802d"
-    }
-  }
+      apiKey: 'AIzaSyD6UhXa92S_dPnTxp-3DrngSHOfU1g7684',
+      authDomain: 'tabby-4af5b.firebaseapp.com',
+      databaseURL: 'https://tabby-4af5b.firebaseio.com',
+      projectId: 'tabby-4af5b',
+      storageBucket: 'tabby-4af5b.appspot.com',
+      messagingSenderId: '849564500568',
+      appId: '1:849564500568:web:750ccd709daae80287802d',
+    },
+  },
+  gapi: {
+    clientId: '849564500568-1t188fdi7ilfnjt2phsmuu4a17h1b4mm.apps.googleusercontent.com',
+    apiKey: 'AIzaSyAn5uMZD7kKUZpaSFAOKV6QX4D7UNA0Zco',
+    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+    scope: 'https://www.googleapis.com/auth/calendar.readonly',
+  },
 };
