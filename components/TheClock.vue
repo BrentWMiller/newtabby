44e2d19e9b5426e26160f9b20ccd3014b7a0ab25
Brent Miller <brentwmiller93@gmail.com>
2020-08-28 23:52:45 -0400

    Styles clock

--- components/TheClock.vue
+++ components/TheClock.vue
@@ -1,25 +1,25 @@
 <template>
-  <div>
-    {{ time }}<br />
-    {{ date }}
-  </div>
+  <datetime class="font-medium text-right">
+    <div class="leading-none uppercase text-32">{{ time }}</div>
+    <div class="text-primary-500 text-18">{{ date }}</div>
+  </datetime>
 </template>
 
 <script>
-import moment from "moment";
+import moment from 'moment';
 
 export default {
   data() {
     return {
-      time: moment().format("h:mm a"),
-      date: moment().format("dddd, MMMM Do")
+      time: moment().format('h:mm a'),
+      date: moment().format('dddd, MMMM Do'),
     };
   },
   mounted() {
     setInterval(() => {
-      this.time = moment().format("h:mm a");
+      this.time = moment().format('h:mm a');
     }, 1000);
-  }
+  },
 };
 </script>
 
